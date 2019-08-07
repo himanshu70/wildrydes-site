714698f0c2de0a0cd7f14d1eb24b9af8d93657c0
himanshu70 <himanshuanandani@gmail.com>
2019-08-07 10:32:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_vh4lf3GE4', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'npvgr47iangd1qqia9ecsg5hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: ' https://keim28dw3m.execute-api.us-west-2.amazonaws.com/PROD' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
